e02ba9ba8374fb710a9894d8d39404258c5687cd
mark <shundyning@outlook.com>
2023-05-03 18:10:39 +0800

    reconstruct project

--- read_epr.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('..')
-
-from EPRReaderPY.eprRead import *
-
-epr_file = '../1-4-2/1-4-2_肝细胞坏死__-_40x.epr'
-epr = EPRread(epr_file)
-a = 1
--- read_slide.py
+++
@@ -1,4 +0,0 @@
-import openslide
-slide_file = '../1-4-2/1-4-2_肝细胞坏死__-_40x.ndpi'
-slide = openslide.OpenSlide(slide_file)
-print(slide.properties['openslide.mpp-y'])
